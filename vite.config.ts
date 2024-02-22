ca36985b0b5f04d3826e267dd997c72968c8da26
felix <felix7051@gmail.com>
2024-02-22 15:14:42 +0000

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/felixngo.github.io/",
+  base: "/",
   server: {
     open: false,
     watch: {
