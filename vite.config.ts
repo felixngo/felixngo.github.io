bc1f615f5695bf8d6fdb023e0ad24b738ed82e84
felix <felix7051@gmail.com>
2024-02-22 14:45:23 +0000

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/LabstepComponents'
+  base: "/felixngo.github.io/",
+  server: {
+    open: false,
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+    port: 80,
+  },
+  build: {
+    outDir: "build",
+    sourcemap: true,
+  },
 })
